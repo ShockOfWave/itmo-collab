4ad28cfc0433fe6a59d1332114ca2159befc65f7
mike-yasunov <mike_yasnov@mail.ru>
2023-09-01 18:59:44 +0300

    Update contours.py

--- api/src/cv/contours.py
+++ api/src/cv/contours.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 from api.src.cv.params import get_center, get_arc_lenght, eq_dia, get_radius, get_area
 from api.src.models.segmentation_model import SegmentationModel
@@ -7,28 +8,31 @@ from api.src.utils.paths import PATH_TO_SEGMENTATION_MODEL
 model = SegmentationModel(model_path=PATH_TO_SEGMENTATION_MODEL)
 
 
-def _highlight_contours(img, contour):
-        cv2.drawContours(img, contour, -1, (0, 255, 0), 2)
+def _highlight_contours(img, contours):
+        for contour in contours:
+            if len(contour):
+                cv2.drawContours(img, [contour], -1, (0, 255, 0), 2)
 
 
 def find_molecules(img):
     contours = model.predict(img)
     if contours is not None and len(contours):
         for contour in contours:
-            cx, cy = get_center(contour)
-            if cx is not None and cy is not None:
-                cv2.circle(img, (cx, cy), 4, (0, 255, 255), -1)
-            arc_lenght = get_arc_lenght(contour)
-            diam = eq_dia(contour)
-            rad = get_radius(contour)
-            area = get_area(contour)
-            text =  f"Area: {int(area)}\nArc lenght: {int(arc_lenght)}\nDiameter: {int(diam)}\nRadius: {int(rad)}"
-            for i, txt in enumerate(text.split('\n')):
-                dy = 15*i
-                cv2.putText(img, txt, (int(cx-rad), int(cy-diam+dy)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,255), 2)
-            cv2.rectangle(img, (cx-int(rad)-5, cy-int(diam)-15), (cx-int(rad)-5+150, cy-int(diam)-15+65), (0, 255, 255), 1)
-            cv2.line(img, (cx, cy), (cx-int(rad)-5+150, cy-int(diam)-15+65), (0,255,255), 1)
-            _highlight_contours(img, contours)
+            if len(contour):
+                cx, cy = get_center(contour)
+                if cx is not None and cy is not None:
+                    cv2.circle(img, (cx, cy), 4, (0, 255, 255), -1)
+                arc_lenght = get_arc_lenght(contour)
+                diam = eq_dia(contour)
+                rad = get_radius(contour)
+                area = get_area(contour)
+                text =  f"Area: {int(area)}\nArc lenght: {int(arc_lenght)}\nDiameter: {int(diam)}\nRadius: {int(rad)}"
+                for i, txt in enumerate(text.split('\n')):
+                    dy = 15*i
+                    cv2.putText(img, txt, (int(cx-rad), int(cy-diam+dy)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,255), 2)
+                cv2.rectangle(img, (cx-int(rad)-5, cy-int(diam)-15), (cx-int(rad)-5+150, cy-int(diam)-15+65), (0, 255, 255), 1)
+                cv2.line(img, (cx, cy), (cx-int(rad)-5+150, cy-int(diam)-15+65), (0,255,255), 1)
+        _highlight_contours(img, contours)
 
 
 
